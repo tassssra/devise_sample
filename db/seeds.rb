c19003683b8a99274ffa167d073f8a8387a75e41
yasuda <tasssra@gmail.com>
2019-04-24 17:49:27 +0900

    ルーティング設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Item.create!(name: "メモの魔力", price: 1500, user_id: 1)
+Item.create!(name: "エンジニアリング組織論", price: 1200, user_id: 1)
+Item.create!(name: "速習Rails", price: 1300, user_id: 1)
